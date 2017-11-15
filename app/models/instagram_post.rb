19ac89098dae20f2887b746b5ad254eabc519bef
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2017-11-15 17:55:56 -0500

    Start parsing of Instagram post json response

--- app/models/instagram_post.rb
+++ app/models/instagram_post.rb
@@ -1,18 +1,62 @@
 class InstagramPost
-  attr_reader :short_code
+  include HTTParty
+
+  attr_accessor(
+    :short_code,
+    :display_url,
+    :username,
+    :profile_pic_url,
+    :like_count,
+    :taken_at_timestamp,
+    :edge_media_to_caption,
+    :edge_media_to_comment_array
+  )
 
   def initialize(short_code:)
     @short_code = short_code
+    fetch_details!
   end
 
   def valid?
-    true
+    @valid
   end
 
   def to_json
     {
-      shortCode: @short_code,
-      userPictureUrl: 'userpic.com'
+      short_code:                  @short_code,
+      display_url:                 @display_url,
+      username:                    @username,
+      profile_pic_url:             @profile_pic_url,
+      like_count:                  @like_count,
+      taken_at_timestamp:          @taken_at_timestamp,
+      edge_media_to_caption:       @edge_media_to_caption,
+      edge_media_to_comment_array: @edge_media_to_comment_array
     }.to_json
+  end
+
+  private
+
+  def fetch_details!
+    response ||= self.class.get("https://www.instagram.com/p/#{short_code}?__a=1")
+
+    if response.success?
+      @response_body ||= response.body
+      @valid = true
+      assign_values!
+    end
+  end
+
+  def assign_values!
+    @parsed ||= JSON.parse(@response_body, symbolize_names: true)
+
+    tap do |post|
+      post.display_url                 = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.username                    = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.profile_pic_url             = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.like_count                  = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.taken_at_timestamp          = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.edge_media_to_caption       = @parsed[:graphql][:shortcode_media][:shortcode]
+      post.edge_media_to_comment_array = @parsed[:graphql][:shortcode_media][:shortcode]
+    end
   end
 end
